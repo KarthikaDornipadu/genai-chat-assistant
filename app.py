05c73e90c2e2443d9c5e536089f40d7339d17b0a
KarthikaDornipadu <dornipadukarthi@gmail.com>
2026-03-08 15:40:12 +0530

    fix render port

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 import math
 
 # Configure Gemini API
-genai.configure(api_key="AIzaSyDPGDAq4UVcmjmRMLLqmkBn8LuKNcmwcak")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = Flask(__name__)
 
@@ -147,5 +147,6 @@ Do NOT hallucinate or make up answers not found in the Context.
         "retrievedChunks": retrieved_chunks_count
     })
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if name == "main":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
